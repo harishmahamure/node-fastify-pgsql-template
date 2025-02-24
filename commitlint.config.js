e690a4ec92b0d8e98e895064c7fc5883793fba5b
Harish Mahamure <hmahamure@gmail.com>
2025-02-24 21:32:34 +0530

    Eslint setup

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-empty": [2, "never"], // Prevents empty type
+    "subject-empty": [2, "never"], // Prevents empty subject
+    "subject-case": [2, "always", "sentence-case"], // Ensures subject is readable
+    "header-max-length": [2, "always", 100], // Max length for commit messages
+  },
+};
